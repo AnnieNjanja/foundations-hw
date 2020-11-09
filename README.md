a6c7a15de364b6ef3151261034dc09c49d1482d7
AnnieNjanja <73808159+AnnieNjanja@users.noreply.github.com>
2020-11-09 22:41:05 +0300

    Updated hellp.py and created README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Foundations Homework
+
+This is homework for 2020's Fall Foundations Data MS program.
--- hw/hello.py
+++ hw/hello.py
@@ -1,4 +1,4 @@
 print('Hello')
 print(3 + 4)
-print('Hello, my friend!!!!')
+print('Hello, my friend!')
 print(20 * 80)
